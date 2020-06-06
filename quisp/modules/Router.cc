caeaeca836bb7634eb100b9c2b0bf6fe6f39a78e
Ravisankar A V <rasa97uchiha@gmail.com>
2020-06-06 08:38:47 +0200

    Merging Code

--- quisp/modules/Router.cc
+++ quisp/modules/Router.cc
@@ -8,6 +8,7 @@
 #include <map>
 #include <omnetpp.h>
 #include <classical_messages_m.h> //Path selection: type = 1, Timing notifier for BMA: type = 4
+#include <algorithm>
 
 using namespace omnetpp;
 using namespace quisp::messages;
@@ -34,6 +35,42 @@ class Router : public cSimpleModule
 };
 
 Define_Module(Router);
+
+void mergePaths(int arr1[], int arr2[]) {
+
+    int m = sizeof(arr1)/sizeof(arr1[0]);
+    int n = sizeof(arr2)/sizeof(arr2[0]);
+
+    // Making sure arr1 is smaller
+    if (m > n) {
+        int *tempp = arr1;
+        arr1 = arr2;
+        arr2 = tempp;
+
+        int temp = m;
+        m = n;
+        n = temp;
+    }
+
+    sort(arr1, arr1 + m);
+
+    for (int i=0; i<n; i++)
+        if (binarySearch(arr1, 0, m-1, arr2[i]) != -1)
+            cout << arr2[i] << " ";
+}
+
+int binarySearch(int arr[], int l, int r, int x){
+    if (r >= l){
+        int mid = l + (r - l)/2;
+
+        if (arr[mid] == x)
+            return mid;
+        if (arr[mid] > x)
+          return binarySearch(arr, l, mid-1, x);
+        return binarySearch(arr, mid+1, r, x);
+    }
+    return -1;
+}
 
 void Router::initialize(int stage)
 {
